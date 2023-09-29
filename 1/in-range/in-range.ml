97e7010cf3ff7aeda75a702fb3362bc66a0a8752
Massimo Bartoletti <bart@unica.it>
2023-09-29 08:38:34 +0200

    minors in_range

--- 1/in-range/in-range.ml
+++ 1/in-range/in-range.ml
@@ -5,5 +5,8 @@ let in_range x a b = (x >= a && x <= b);;
 assert(in_range (5) 0 10);;
 assert(not (in_range (-4) 0 10));;
 
-(* in_range : int -> int -> int -> bool *)
-let in_range (x:int) a b = (x >= a && x <= b);;
+(* in_range2 : int -> int -> int -> bool *)
+let in_range2 (x:int) a b = (x >= a && x <= b);;
+
+assert(in_range2 (5) 0 10);;
+assert(not (in_range2 (-4) 0 10));;
