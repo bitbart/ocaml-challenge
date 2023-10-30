a2002f3f33e52144cf4041f7a102d50c0c29c2ea
Massimo Bartoletti <bart@unica.it>
2023-10-30 23:48:13 +0100

    added countzero.ml

---
+++ 3/countzero/countzero.ml
@@ -0,0 +1,10 @@
+let rec countzero f a b =
+  if b<a then 0 else (if f a = 0 then 1 else 0) + countzero f (a+1) b;;
+
+assert (countzero (fun x -> x) (-10) 10 = 1);;
+
+assert (countzero (fun x -> x) 1 10 = 0);;
+
+assert (countzero (fun x -> x*x - 1) (-10) 10 = 2);;
+
+assert (countzero (fun x -> (if x<0 then -x else x) - 1) (-10) 10 = 2);;
