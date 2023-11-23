35b371d6a4a44809484c4d7528e8988291df3633
Massimo Bartoletti <bart@unica.it>
2023-11-23 08:54:16 +0100

    revised maxnat

--- 1/maxnat/maxnat.ml
+++ 1/maxnat/maxnat.ml
@@ -1,3 +1,10 @@
 let max_nat a b = 
-  assert (a>=0 && b>=0);
-  if a<b then b else a;;
+  if not (a>=0 && b>=0)
+  then failwith "Error"
+  else if a<b then b else a;;
+
+assert(max_nat 2 5 = 5);;
+assert(max_nat 5 2 = 5);;
+assert(try max_nat (-2) 5 |> fun _ -> false with _ -> true);;
+assert(try max_nat 2 (-5) |> fun _ -> false with _ -> true);;
+assert(try max_nat (-2) (-5) |> fun _ -> false with _ -> true);;
